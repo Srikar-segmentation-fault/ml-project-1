2cf190949cd9407e27ec4020dd7f5210d79163f6
Srikar-segmentation-fault <psrikar2005@gmail.com>
2025-10-20 23:48:44 +0530

    EDA , prblm statmnt and model training

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
-import logging
+from logger import logging
+
+
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -13,9 +15,15 @@ def error_message_detail(error,error_detail:sys):
     
     
 class CustomException(Exception):
-    def __init__(self,error_msg,error_detail:sys):
+    def __init__(self, error_msg, error_detail:sys):
         super().__init__(error_msg)
         self.error_msg=error_message_detail(error_msg,error_detail=error_detail)
     def __str__(self):
         return self.error_msg
 
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e,sys)
